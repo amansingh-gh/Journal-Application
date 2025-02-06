e352efad947d5adb33227d628e72dd21287cb190
Aman Singh <yo.akaman@gmail.com>
2025-02-06 22:31:32 +0530

    update

--- src/main/java/net/singh/journalApp/config/SecurityConfig.java
+++ src/main/java/net/singh/journalApp/config/SecurityConfig.java
@@ -1,16 +1,28 @@
 package net.singh.journalApp.config;
 
+import net.singh.journalApp.service.UserDetailsServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private final UserDetailsServiceImp userDetailsServiceImp;
+
+    public SecurityConfig(UserDetailsServiceImp userDetailsServiceImp) {
+        this.userDetailsServiceImp = userDetailsServiceImp;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -20,7 +32,17 @@ public class SecurityConfig {
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.disable()) // Disables CSRF (only if necessary)
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/net/singh/journalApp/service/UserDetailsServiceImp.java
+++ src/main/java/net/singh/journalApp/service/UserDetailsServiceImp.java
@@ -1,29 +1,29 @@
-//package net.singh.journalApp.service;
-//
-//import net.singh.journalApp.repository.UserRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Component;
-//import org.springframework.stereotype.Service;
-//
-//@Component
-//public class UserDetailsServiceImp implements UserDetailsService {
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        User user = userRepository.findByUsername(username);
-//        if (user != null) {
-//            User.builder()
-//                    .username(username)
-//                    .password(user.getPassword())
-//                    .roles(user.getRoles().toArray(new String[0]))
-//                    .build();
-//        }
-//    }
-//}
+package net.singh.journalApp.service;
+
+import net.singh.journalApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+public class UserDetailsServiceImp implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        net.singh.journalApp.entity.User user = userRepository.findByUsername(username);
+        if (user != null) {
+            return org.springframework.security.core.userdetails.User.builder()
+                    .username(user.getUsername())
+                    .password(user.getPassword())
+                    .roles(user.getRole().toArray(new String[0]))
+                    .build();
+        }
+        throw new UsernameNotFoundException("User not found " + username);
+    }
+}
